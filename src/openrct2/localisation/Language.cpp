886f435bb96f3e7660c07b064e3b2579363ac60b
qqkookie <39845532+qqkookie@users.noreply.github.com>
2018-07-27 23:58:15 +0900

    modified:   src/openrct2/drawing/Font.cpp 	modified:   src/openrct2/localisation/FormatCodes.h 	modified:   src/openrct2/localisation/Language.cpp 	modified:   src/openrct2/sprites.h

--- src/openrct2/localisation/Language.cpp
+++ src/openrct2/localisation/Language.cpp
@@ -43,11 +43,7 @@ const language_descriptor LanguagesDescriptors[LANGUAGE_COUNT] =
     { "nb-NO", "Norwegian",             "Norsk",                 FAMILY_OPENRCT2_SPRITE,                RCT2_LANGUAGE_ID_ENGLISH_UK },          // LANGUAGE_NORWEGIAN
     { "pl-PL", "Polish",                "Polski",                FAMILY_OPENRCT2_SPRITE,                RCT2_LANGUAGE_ID_ENGLISH_UK },          // LANGUAGE_POLISH
     { "pt-BR", "Portuguese (BR)",       u8"Português (BR)",      FAMILY_OPENRCT2_SPRITE,                RCT2_LANGUAGE_ID_PORTUGUESE },          // LANGUAGE_PORTUGUESE_BR
-<<<<<<< HEAD
     { "ru-RU", "Russian",               u8"Русский",             FAMILY_OPENRCT2_SPRITE,                RCT2_LANGUAGE_ID_ENGLISH_UK },          // LANGUAGE_RUSSIAN
-=======
-    { "ru-RU", "Russian",               "Russian",               FAMILY_OPENRCT2_SPRITE,                RCT2_LANGUAGE_ID_ENGLISH_UK },          // LANGUAGE_RUSSIAN
->>>>>>> Add sprite glyphs for Cyrillic/Russian
     { "fi-FI", "Finnish",               "Suomi",                 FAMILY_OPENRCT2_SPRITE,                RCT2_LANGUAGE_ID_ENGLISH_UK },          // LANGUAGE_FINNISH
     { "sv-SE", "Swedish",               "Svenska",               FAMILY_OPENRCT2_SPRITE,                RCT2_LANGUAGE_ID_SWEDISH },             // LANGUAGE_SWEDISH
 };
