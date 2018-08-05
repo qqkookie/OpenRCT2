001980d906ddda1a18bf63478b44d76f9036aeee
qqkookie <39845532+qqkookie@users.noreply.github.com>
2018-08-05 23:10:12 +0900

    Fix weekly profit mismatch with monthly profit.

--- src/openrct2-ui/windows/Finances.cpp
+++ src/openrct2-ui/windows/Finances.cpp
@@ -1048,8 +1048,9 @@ static void window_finances_profit_graph_paint(rct_window* w, rct_drawpixelinfo*
     graphRight = w->x + pageWidget->right - 4;
     graphBottom = w->y + pageWidget->bottom - 4;
 
-    // Weekly profit
-    money32 weeklyPofit = gCurrentProfit;
+    // Weekly profit approximation
+    money32 weeklyPofit = ( gWeeklyProfitAverageDivisor != 0 ?
+        (7 * gWeeklyProfitAverageDividend / gWeeklyProfitAverageDivisor) : gWeeklyProfitAverageDividend);
     gfx_draw_string_left(
         dpi, weeklyPofit >= 0 ? STR_FINANCES_WEEKLY_PROFIT_POSITIVE : STR_FINANCES_WEEKLY_PROFIT_LOSS, &weeklyPofit,
         COLOUR_BLACK, graphLeft, graphTop - 11);
